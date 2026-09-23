8a1821e4c2ec0fb483c18e8738231093ecbd42b3
qwertyuiopok2 <steam656op@gmail.com>
2026-09-23 22:35:25 +0300

    Закрепил материал прошлого занятия

--- Program.cs
+++ Program.cs
@@ -15,90 +15,112 @@ using System.Xml;
 using Microsoft.VisualBasic;
 namespace c_larp;
 
-    class Program
-   {     
-        static void  Main (string[] args)
-        {
-          List<Biboba> Zov = new List<Biboba>();
-          Zov.Add(new Biboba {Name = "Artem", Status = "Admin", Time = 10, City = "Москва" });
-          Zov.Add(new Biboba {Name = "Max", Status = "Guest", Time = 11, City = "Екатеринбург" });
-          Zov.Add(new Biboba {Name = "Micha", Status = "Admin", Time = 5, City = "Орехово-Зуево" });
-          Zov.Add(new Biboba {Name = "Liza", Status = "Admin", Time = 6, City = "Москва" });
-          Zov.Add(new Biboba {Name = "Lesha", Status = "Guest", Time = 15, City = "Москва" });
-
-
-            
-
-          while(true)
-          {
-            Console.WriteLine("Система контроля доступа");
-            Console.WriteLine("1 - Показать все логи");
-            Console.WriteLine("2 - Показать всех админов из Москвы");
-            Console.WriteLine("3 - Удалить всех гостей пришедших после 10");
-            Console.WriteLine("4 - Выход из программы ");
-            Console.WriteLine("\n Выберите опцию");
-            Processmenu(Zov);
-          }
-            
-        static  void Processmenu (List<Biboba> lox)
-        {
-            string? comand = Console.ReadLine();
-            switch (comand)
-            {
-                case "1":
-                    for (int i = 0; i < lox.Count ; i++)
-                    {
-                        Console.WriteLine($"Сотрудник -{lox[i].Name} , Статус -{lox[i].Status}, Время -{lox[i].Time} , Город -{lox[i].City} ");
-                    }
-                    Console.WriteLine("нажмите кнопку для завершения программы");
-                    Console.ReadKey();
-                    break;
-                case "2":
-                    for (int i = 0; i < lox.Count; i++)
-                    {
-                        if (lox[i].Status == "Admin" && lox[i].City =="Москва")
-                            Console.WriteLine($"Сотрудник {lox[i].Name} из города {lox[i].City}");
-                        }           
-                    Console.WriteLine("Для выхода из программы нажмите любую кнопку");
-                    Console.ReadKey();            
-                    break;
-                case "3":
-                    for (int i = lox.Count - 1; i>= 0 ;  i--) // от конца к началу нужно для удаления элементов и проверка начинается с конца в начало чтобы после удаления
-                    // элементов те которые сдвинутся влево не заставили цикл пропустить шаги
-                    {
-                        if (lox[i].Status == "Guest" && lox[i].Time > 10)
-                        {
-                            Console.WriteLine($"Удален гость: {lox[i].Name}");
-                            lox.RemoveAt(i);
-                        }
-                    }
-                    break;
-                case "4":
-                    Console.WriteLine("Введите команду - stop - для выхода из программы");
-                    string? leave = Console.ReadLine();
-                    if (leave == "stop")
-                    {
-                       Environment.Exit(0); //выйти из среды
-                    }
-                    break;
-
-            }
-        }
-        }    
-    }
-
-public class Biboba
+public class OchkoPredmet
 {
     public string? Name { get; set; } = string.Empty;
-    public string? Status { get; set; } = string.Empty;
-    public int Time { get; set; }
-    public string? City { get; set; } = string.Empty;
+    public string? Redkost { get; set; } = string.Empty;
+    public int Damage { get; set; }
 }
 
+class Inventory
+{
+    static void Main(string[] lox)
+    {
+        List<OchkoPredmet> inventory = new List<OchkoPredmet>();
 
+        inventory.Add(new OchkoPredmet{Name = "ржавый меч", Redkost = "Basic", Damage = 2});
+        inventory.Add(new OchkoPredmet{Name = "Палочка", Redkost = "Rare", Damage = 10});
+        inventory.Add(new OchkoPredmet{Name = "Рапира", Redkost = "Legendary", Damage = 50});
+        while (true)
+        {
+        Console.WriteLine("-- Ваш инвентарь --");
+        Console.WriteLine("-- 1 - Показать инвентарь --");
+        Console.WriteLine("-- 2 - Показать только легендарные предметы --");
+        Console.WriteLine("-- 3 - Добавить предмет в инвентарь --");
+        Console.WriteLine("-- 4 - Выйти из инвентаря --");
+        Console.WriteLine("-- Выберите действие --");
 
+        string? op = Console.ReadLine();
 
+        switch (op)
+        {
+            case "1":
+            {
+                for (int i = 0; i < inventory.Count; i++)
+                {
+                    Console.WriteLine($"{inventory[i].Name} с редкостью {inventory[i].Redkost}, с дополнительным уроном {inventory[i].Damage} ");
+                }
+                Console.WriteLine("ВАШ ИНВЕНТАРЬ");
+                break;
+            }
 
+            case "2":
+            {
+                for (int i = 0; i < inventory.Count; i++)
+                {
+                    if (inventory[i].Redkost == "Legendary")
+                    {
+                        Console.WriteLine($"\n С легендарной редкостью предмет {inventory[i].Name}  с доп уроном {inventory[i].Damage}");
+                    }
+                }
+                break;
+            }
 
-   
-             
+            case "3":
+            {
+                Console.WriteLine("Введите название предмета, которого хотим добавить");
+                string? newitem = Console.ReadLine();
+                if (newitem != null)
+                {
+                    Console.WriteLine("Введите редкость предмета, которого хотим добавить: Basic, Rare, Legendary");
+                    string? newredkost = Console.ReadLine();
+                    if (newredkost == "Basic" || newredkost == "Rare" || newredkost == "Legendary")
+                    {
+                        Console.WriteLine("Введите сколько дополнительного урона вносит данное оружие");
+                        string? num = Console.ReadLine();
+                        if(num != null)
+                        {
+                            if (int.TryParse(num, out int num1) == true)
+                            {
+                                inventory.Add(new OchkoPredmet{Name = newitem, Redkost = newredkost, Damage = num1});
+                                break;
+                            }
+                            else
+                            {
+                                Console.WriteLine("ОШИБКА");
+                                Console.ReadKey();
+                                return;
+                            }
+
+                        }
+                        else 
+                        {
+                            Console.WriteLine("ОШИБКА");
+                            Console.ReadKey();
+                            return;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("ОШИБКА");
+                        Console.ReadKey();
+                        return;
+                    }
+                } 
+                else 
+                {
+                    return;
+                }               
+
+            }
+
+            case "4":
+            {
+                Console.WriteLine($"\n Для выхода из программы нажимет любую кнопку");
+                Console.ReadKey();
+                return;
+            }
+        }
+        }
+    }
+}
