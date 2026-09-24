04bca94f8bc7a68c5bee826497c780cff4ce17d7
qwertyuiopok2 <steam656op@gmail.com>
2026-09-24 20:47:23 +0300

    добавил логику экипировки предметов, которые есть в инвентаре

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ public class OchkoPredmet
     public string? Name { get; set; } = string.Empty;
     public string? Redkost { get; set; } = string.Empty;
     public int Damage { get; set; }
+    public bool Iseq {get; set;} = false; //true в руках false в рюкзаке
 }
 
 class Inventory
@@ -24,7 +25,8 @@ class Inventory
         Console.WriteLine("-- 1 - Показать инвентарь --");
         Console.WriteLine("-- 2 - Показать только легендарные предметы --");
         Console.WriteLine("-- 3 - Добавить предмет в инвентарь --");
-        Console.WriteLine("-- 4 - Выйти из инвентаря --");
+        Console.WriteLine("-- 4 - Взять предмет в руки --");
+        Console.WriteLine("-- 5 - Выйти из инвентаря --");
         Console.WriteLine("-- Выберите действие --");
 
         string? op = Console.ReadLine();
@@ -35,7 +37,9 @@ class Inventory
             {
                 for (int i = 0; i < inventory.Count; i++)
                 {
-                    Console.WriteLine($"{inventory[i].Name} с редкостью {inventory[i].Redkost}, с дополнительным уроном {inventory[i].Damage} ");
+                    string eq = inventory[i].Iseq ? "[В руках]" : ""; // ? заменяет  , а : заменяет elif
+
+                    Console.WriteLine($"{inventory[i].Name} с редкостью {inventory[i].Redkost}, с дополнительным уроном {inventory[i].Damage} - {eq} ");
                 }
                 Console.WriteLine("ВАШ ИНВЕНТАРЬ");
                 break;
@@ -107,8 +111,39 @@ class Inventory
                 Console.ReadKey();
                 break;
             }
-
             case "4":
+            {
+                Console.WriteLine("Экипировка оружия");
+                Console.WriteLine("Введите точное название предмета, который хотите взять в руки");
+                string? eqname = Console.ReadLine();
+                bool itemfound = false;
+                for (int i = 0; i < inventory.Count; i++) // Проверяем весь список чтобы была информация о всех предметах и соответственно если название предмета в массиве существует оно может быть экипировано то есть введено нами чтобы мы могли его экипировать
+                {
+                    if (inventory[i].Name == eqname)
+                    {
+                        for (int j = 0; j < inventory.Count; j++) // внутренний цикл обнуляющий весь инвентарь после ввода того предмета который мы хотим экипировать (поменять статус на true)
+                        {
+                            inventory[j].Iseq = false;
+                        }
+                        inventory[i].Iseq = true;
+                        itemfound = true;
+                        break;
+                    }
+                }
+                
+                if (itemfound != true)
+                {
+                    Console.WriteLine("Предмет не найден ошибка");
+                }
+                else
+                {
+                    Console.WriteLine("предмет добавлен");
+                }
+                    Console.ReadKey();
+                    break;
+            }
+
+            case "5":
             {
                 Console.WriteLine($"\n Для выхода из программы нажимет любую кнопку");
                 Console.ReadKey();
